fbb3a1a5162391a0ca9c8eded658b165cac910e2
UNCLEBREAKER <45223997+TitoUncle123@users.noreply.github.com>
2019-03-07 15:06:36 +0800

    DB_Codes
    db_codes

---
+++ Log-in.php
@@ -0,0 +1,27 @@
+<?php
+   include("mysqli_connect.php");
+   session_start();
+   
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+      
+      $myusername = mysqli_real_escape_string($db,$_POST['username']);
+      $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
+      
+      $sql = "SELECT id FROM admin WHERE uname = '$myusername' and psw = '$mypassword'";
+      $result = mysqli_query($db,$sql);
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+      $active = $row['active'];
+      
+      $count = mysqli_num_rows($result);
+      
+		
+      if($count == 1) {
+         session_register("myusername");
+         $_SESSION['login_user'] = $myusername;
+         
+         header("location: welcome.php");
+      }else {
+         $error = "Your Login Name or Password is incorrect";
+      }
+   }
+?>
